b77e6064c002c7b844db810ff83a9fbf0c644554
fitboye <49235923+fitboye@users.noreply.github.com>
2024-01-27 22:20:43 -0500

    dk

--- bots/epic_bot2.py
+++ bots/epic_bot2.py
@@ -14,6 +14,7 @@ class BotPlayer(Player):
         self.reinforcer_count = 1
         self.gf_ratio = 1
         self.gb_ratio = 2
+        self.turn = 0
 
     def paths_in_range(self, map: Map, x: int, y: int, tower):
         res = 0
@@ -26,10 +27,23 @@ class BotPlayer(Player):
         if (tower == TowerType.SOLAR_FARM):
             return -res
         return res
+    
+    def sell_bombers(self, rc):
+        towers = rc.get_towers(rc.get_ally_team())
+        for tower in towers:
+            if tower.type == TowerType.GUNSHIP:
+                rc.sell_tower(tower.id)
 
     def play_turn(self, rc: RobotController):
-        self.build_towers(rc)
+        print(self.turn)
+        if (self.turn == 3000):
+            self.sell_bombers(rc)
+            self.gb_ratio = 9999999999999999999
+            self.bomb_count = 1
+        while (self.build_towers(rc) == 1):
+            self.build_towers(rc)
         self.towers_attack(rc)
+        self.turn += 1
 
     def build_towers(self, rc: RobotController):
         tower = TowerType.GUNSHIP #what tower to build?
@@ -38,13 +52,13 @@ class BotPlayer(Player):
         if (self.gunship_count / self.farm_count >= self.gf_ratio):
             tower = TowerType.SOLAR_FARM
         if rc.get_balance(rc.get_ally_team()) < tower.cost:
-            return
+            return 0
 
         map = rc.get_map()
         maxpaths = -99999999999999999
         coords = (0, 0)
-        for i in range(map.height):
-            for j in range(map.width):
+        for i in range(map.width):
+            for j in range(map.height):
                 if not (map.is_space(i, j) and rc.is_placeable(rc.get_ally_team(), i, j)): continue
                 paths = self.paths_in_range(map, i, j, tower)
                 if paths > maxpaths:
@@ -52,11 +66,14 @@ class BotPlayer(Player):
                     coords = (i,j)
         x,y = coords
 
+        if (maxpaths == -99999999999999999): return 0
+
         rc.build_tower(tower, x, y)
         if (tower == TowerType.GUNSHIP): self.gunship_count += 1
         elif (tower == TowerType.BOMBER): self.bomb_count += 1
         elif (tower == TowerType.SOLAR_FARM): self.farm_count+=1
         else: self.reinforcer_count += 1
+        return 1
 
     
     def towers_attack(self, rc: RobotController):
